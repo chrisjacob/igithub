a51f51431fd0be9020200c5f3d83381ab9e144b8
Chris Jacob <i.chris.jacob@gmail.com>
2010-06-03 14:32:25 +1000

    updated application template to jQuery r148 requirments

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,25 +1,58 @@
 var jQT = new $.jQTouch({
+	icon: 'images/jqtouch.png',
+	addGlossToIcon: false,
+	startupScreen: 'images/jqt_startup.png',
 	preloadImages: [
+		'jqtouch/themes/apple/img/actionButton.png',
 		'jqtouch/themes/apple/img/backButton.png',
-		'jqtouch/themes/apple/img/blueButton.png',
-		'jqtouch/themes/apple/img/whiteButton.png',
-		'jqtouch/themes/apple/img/loading.gif'
+		'jqtouch/themes/apple/img/chevron.png',
+		'jqtouch/themes/apple/img/loading.gif',
+		'jqtouch/themes/apple/img/pinstripes.png',
+		'jqtouch/themes/apple/img/selection.png',
+		'jqtouch/themes/apple/img/toolbar.png',
+		'jqtouch/themes/apple/img/toolButton.png'
 	]
 });
 
 $(function(){
 
+	$('#tapme').tap(function(){
+		$(this).parent().after('<li>tapped!</li>')
+	})
+	
+	$('a[target="_blank"]').click(function() {
+		if (confirm('This link opens in a new window.')) {
+			return true;
+		} else {
+		return false;
+		}
+	});
+	
+	// Page animation callback events
+	$('#pageevents').
+		bind('pageAnimationStart', function(e, info){ 
+			$(this).find('.info').append('Started animating ' + info.direction + '&hellip; ');
+		}).
+		bind('pageAnimationEnd', function(e, info){
+			$(this).find('.info').append(' finished animating ' + info.direction + '.<br /><br />');
+		});
+	
 	// Page animations end with AJAX callback event, example 1 (load remote HTML only first time)
 	$('#callback').bind('pageAnimationEnd', function(e, info){
-		if (!$(this).data('loaded')) {                      // Make sure the data hasn't already been loaded (we'll set 'loaded' to true a couple lines further down)
-			$(this).append($('<div>Loading</div>').         // Append a placeholder in case the remote HTML takes its sweet time making it back
-				load('jqtouch/demos/igithub/ajax.html .info', function() {        // Overwrite the "Loading" placeholder text with the remote HTML
-					$(this).parent().data('loaded', true);  // Set the 'loaded' var to true so we know not to re-load the HTML next time the #callback div animation ends
+		// Make sure the data hasn't already been loaded (we'll set 'loaded' to true a couple lines further down)
+		if (!$(this).data('loaded')) {
+			// Append a placeholder in case the remote HTML takes its sweet time making it back
+			// Then, overwrite the "Loading" placeholder text with the remote HTML
+			$(this).append($('<div>Loading</div>').load('ajax.html .info', function() {        
+				// Set the 'loaded' var to true so we know not to reload
+				// the HTML next time the #callback div animation ends
+				$(this).parent().data('loaded', true);  
 			}));
 		}
 	});
 
-	// TESTING
+
+	// API TESTING
 	var runtests = true;
 	var fullstack = true;
 	
